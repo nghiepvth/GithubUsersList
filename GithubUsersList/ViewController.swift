3bc586e473d0674c6f313c731b9f48d94be2b1cc
Stagit Stephan <nghiepvth@gmail.com>
2022-01-28 11:13:54 +0900

    Initial Commit

---
+++ GithubUsersList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubUsersList
+//
+//  Created by Stagit Stephan on 2022/01/28.
+//  Copyright © 2022 Stagit Stephan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
